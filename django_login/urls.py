24c7a59d295aedefd90b3a587db883d56a350411
PNNutkung <pipatpol.t@ku.th>
2016-09-28 23:01:43 +0700

    Add html templates

--- django_login/urls.py
+++ django_login/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from login import views
 
 urlpatterns = [
     url(r'^', include('login.urls')),
--- login/views.py
+++ login/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from django.urls import reverse
 
 # Create your views here.
 def index(req):
-    return HttpResponse('Hello, world. You\'re at the polls index.')
+    return render(req, 'login/index.html')
+
+def login(req):
+    return None
+
+def not_found(req):
+    raise Http404('Page not found!')
