bdbf1d34d2815c16990d161d51125c1deef799a8
PNNutkung <pipatpol.t@ku.th>
2016-09-28 21:08:23 +0700

    Change database

--- django_login/settings.py
+++ django_login/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'django_login.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_login_test',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'USER': 'root',
+        'PASSWORD': '',
     }
 }
 
